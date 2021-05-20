03bd21af3c51df81af3f6292785eb1724333f8c7
Girik Bajaj <bajajgirik2010@gmail.com>
2021-05-20 14:24:12 +0530

    Taking input from user and generated it's url for google search

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from bs4 import BeautifulSoup
 import webbrowser
 from time import sleep
 
-# The first url is the one we check the product availability
-# The rest three are for testing purposes only
+def searchQuery(str):
+    str += '+'
+    return str
 
-url = "https://www.flipkart.com/sony-playstation-5-cfi-1008a01r-825-gb-astro-s-playroom/p/itma0201bdea62fa"
-url2 = "https://www.flipkart.com/msks-144-tc-cotton-double-printed-bedsheet/p/itm04a52ea36a410?pid=BDSGF8YRZEYGEDXV&lid=LSTBDSGF8YRZEYGEDXVNGMHS1&marketplace=FLIPKART&store=jra%2Fknw%2Fqcw&srno=b_1_1&otracker=hp_omu_Deals%2Bof%2Bthe%2BDay_1_4.dealCard.OMU_PD9WSIRXSPA4_3&otracker1=hp_omu_SECTIONED_manualRanking_neo%2Fmerchandising_Deals%2Bof%2Bthe%2BDay_NA_dealCard_cc_1_NA_view-all_3&fm=neo%2Fmerchandising&iid=en_WvWUDpE4cg6%2BbTWo39j8d%2FUB8fmGSkMyLZW5UjqoJXurphmxdX3q%2BZSklwLr%2B6h12IrxNew8UN5k8hrDPCz3TQ%3D%3D&ppt=browse&ppn=browse&ssid=k0fgkllltc0000001621439178255"
-url3 = "https://www.flipkart.com/tabaret-sup-400-1-games-retro-game-box-console-8-gb-fifa-14/p/itm1efae0ce157b7?pid=GMCFKG7UUNW7TF3S&lid=LSTGMCFKG7UUNW7TF3SW0CMNE&marketplace=FLIPKART&q=ps4&store=4rr%2Fx1m%2Fwym&srno=s_1_3&otracker=search&otracker1=search&fm=SEARCH&iid=c53fba62-b775-4d9e-aaf3-c1a8a169a919.GMCFKG7UUNW7TF3S.SEARCH&ppt=sp&ppn=sp&ssid=vww7veh05c0000001621445712344&qH=0e740342f31e9aa6"
-url4 = "https://www.flipkart.com/airtel-amf-311ww-data-card/p/itm74cfab0443198?pid=DATFUKXTCGXQUC3Y&lid=LSTDATFUKXTCGXQUC3YEQYRYT&marketplace=FLIPKART&store=6bo%2Fo47&srno=b_1_1&otracker=hp_omu_Top%2BDeals%2Bon%2BElectronics_2_10.dealCard.OMU_72KF9YP4INE7_6&otracker1=hp_omu_WHITELISTED_neon%2Fmerchandising_Top%2BDeals%2Bon%2BElectronics_NA_dealCard_cc_2_NA_view-all_6&fm=neon%2Fmerchandising&iid=2f63bbc7-f2e2-43c1-ae17-d253a4535653.DATFUKXTCGXQUC3Y.SEARCH&ppt=hp&ppn=homepage&ssid=wicfiw88gw0000001621447286257"
+n = input().split()
+new_ns = list(map(searchQuery,n[:-1]))
+url = 'https://www.google.com/search?q='
+
+for new_n in new_ns:
+    url += new_n
+
+url += n[-1]
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
